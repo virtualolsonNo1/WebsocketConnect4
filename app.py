3ab47596a5b9ba10c8d4c0d167c6b8e725b68661
Carl Olson <virtualolson.no1@gmail.com>
2025-02-19 14:43:54 -0600

    Winner found !!!!!!!!!!!!!!!!!!!!!!!

--- app.py
+++ app.py
@@ -34,31 +34,19 @@ async def handler(websocket):
                 "column": message["column"],
                 "row": row,
             }
+
             await websocket.send(json.dumps(event))
             await asyncio.sleep(0.5)
+            
+            if game.winner is not None:
+                event = {
+                    "type": "win",
+                    "player": game.last_player,
+                }
+            
+            await websocket.send(json.dumps(event))
 
-    # for player, column, row in [
-    #     (PLAYER1, 3, 0),
-    #     (PLAYER2, 3, 1),
-    #     (PLAYER1, 4, 0),
-    #     (PLAYER2, 4, 1),
-    #     (PLAYER1, 2, 0),
-    #     (PLAYER2, 1, 0),
-    #     (PLAYER1, 5, 0),
-    # ]:
-    #     event = {
-    #         "type": "play",
-    #         "player": player,
-    #         "column": column,
-    #         "row": row,
-    #     }
-    #     await websocket.send(json.dumps(event))
-    #     await asyncio.sleep(0.5)
-    # event = {
-    #     "type": "win",
-    #     "player": PLAYER1,
-    # }
-    # await websocket.send(json.dumps(event))
+
 
 async def main():
     async with serve(handler, "", 8001) as server:
